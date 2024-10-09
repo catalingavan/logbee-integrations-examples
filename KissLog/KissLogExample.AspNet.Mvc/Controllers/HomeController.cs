238fd78f42cd202c5200033dd1884f33b9eb6c19
Catalin Gavan <catalingavan@gmail.com>
2024-10-09 16:12:26 +0200

    KissLog ASP.NET MVC example

---
+++ KissLog/KissLogExample.AspNet.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using KissLog;
+using System.Web.Mvc;
+
+namespace KissLogExample.AspNet.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IKLogger _logger;
+        public HomeController()
+        {
+            _logger = Logger.Factory.Get();
+        }
+
+        public ActionResult Index()
+        {
+            _logger.Debug("Hello World! from ASP.NET MVC ");
+            return View();
+        }
+    }
+}
